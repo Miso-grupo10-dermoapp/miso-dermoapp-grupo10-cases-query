122d05f9d012ea8ba664f7a9ebb4ec0b97c7d647
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-18 20:49:06 -0500

    adding all cases logic  and test

--- src/app.py
+++ src/app.py
@@ -10,10 +10,10 @@ ENV_TABLE_NAME = "dermoapp-patient-cases"
 def handler(event, context):
     try:
         print("lambda execution with context {0}".format(str(context)))
-        if validate_property_exist("patient_id", event['pathParameters']) and validate_property_exist("case_id", event['pathParameters']):
-                response = get_item("case_id", event['pathParameters']['case_id'])
+        if validate_property_exist("patient_id", event['pathParameters']):
+                response = get_item(event['pathParameters']['patient_id'])
                 return return_status_ok(response)
         else:
-            return return_error_response("missing or malformed request body", 412)
+            return return_error_response("missing or malformed query params", 412)
     except Exception as err:
         return return_error_response("cannot proceed with the request error: " + str(err), 500)
